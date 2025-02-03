8d7210ca706ada672ce5d657bafda3052cea75db
Moheeb <moheebk522@gmail.com>
2025-02-03 12:40:26 +0530

    add register route

--- src/app.js
+++ src/app.js
@@ -14,4 +14,7 @@ app.use(express.urlencoded({limit: "16kb", extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
+import router from "./routes/user.routes.js";
+app.use("/api/v1/users", router)
+
 export default app;
